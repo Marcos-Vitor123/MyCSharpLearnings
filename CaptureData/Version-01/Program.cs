7d55c61e1764e60263ac7a88c812a4d90bc0a381
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-24 02:25:19 -0300

    Capturar dados - versão 01

---
+++ CaptureData/Version-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string capturar = Console.ReadLine();
+            Console.WriteLine($"Olá, {capturar}");
+
+            Console.ReadKey();
+        }
+    }
+}
