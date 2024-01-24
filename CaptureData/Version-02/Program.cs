dc191277c07b0b893373eb6b112c43c4e35651cd
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-24 02:29:43 -0300

    Capturar dados - versão 02

---
+++ CaptureData/Version-02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+
+            int capturar = Convert.ToInt32(Console.ReadLine()); // Coversão para inteiro, pois é padrão gerar string
+
+            Console.WriteLine($"O número é: {capturar}");
+
+            Console.ReadKey();
+        }
+    }
+}
