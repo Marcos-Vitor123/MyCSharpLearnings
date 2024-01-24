adb61aef293becf845944b214f59d9064eb68d34
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-24 01:55:43 -0300

    Funções - versão 02

--- functions/Version-01/Program.cs
+++ functions/Version-01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int resultado = Soma(2, 3);
+
+            int Soma(int a, int b)
+            {
+                return a + b;
+            }
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
+
---
+++ functions/Version-02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           int resultado = Subtrair(5, 10);
+
+           Console.WriteLine(resultado);
+        }
+        static int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
