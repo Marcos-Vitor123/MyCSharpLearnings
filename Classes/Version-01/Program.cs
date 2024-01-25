c1301af0468045aba3a4c0349675e429643a82c1
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-25 19:52:17 -0300

    Classes - versão 01

---
+++ Classes/Version-01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           Pessoa pessoa = new Pessoa();
+           pessoa.nome = "Marcos";
+           pessoa.idade = 34;
+
+           Console.WriteLine($"Nome: {pessoa.nome}\nIdade: {pessoa.idade}");
+        }
+    }
+
+    class Pessoa
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+    }
+}
