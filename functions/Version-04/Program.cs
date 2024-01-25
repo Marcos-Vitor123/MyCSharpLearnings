a4ff5fec931c5d6676500accdff38a154444fa05
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-25 00:36:39 -0300

    Funções - versão 04

---
+++ functions/Version-04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ExibirString("abcdefgh");
+        }
+        public static void ExibirString(string exibirString) // função sem retorno
+        {
+            Console.WriteLine(exibirString);
+        }
+    }
+}
