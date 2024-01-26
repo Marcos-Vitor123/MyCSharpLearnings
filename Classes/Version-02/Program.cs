a8392b021d82a572697ed5be52bb0dd1ce954480
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-26 19:49:08 -0300

    Classes - versão 03

--- Classes/Version-02/Program.cs
+++ Classes/Version-02/Program.cs
@@ -7,6 +7,7 @@ namespace Classes
         private static void Main(string[] args)
         {
             Carro carro = new Carro();
+            
             carro.marca = "Ford";
             carro.modelo = "Ford Fusion";
             carro.cor = "Preto";
@@ -21,7 +22,7 @@ namespace Classes
         public string marca { get; set; }
         public string modelo { get; set; }
         public string cor { get; set; }
-        public int ano{ get; set; }
+        public int ano { get; set; }
         public int velocidade { get; set; } 
     }
 }
---
+++ Classes/Version-03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Animal
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            animal.nome = "Havalão";
+            animal.idade = 10;
+            animal.especie = "Cavalo";
+            animal.altura = 2.75f;
+            animal.peso = 150f;
+
+            Console.WriteLine($"Nome: {animal.nome}\nIdade: {animal.idade}\nEspécie: {animal.especie}\nAltura: {animal.altura}\nPeso: {animal.peso}");
+        }
+    }
+
+    class Animal
+    {
+        public string nome { get; set; }
+        public string especie { get; set; }
+        public float peso { get; set; }
+        public float altura { get; set; }
+        public int idade { get; set; }
+    }
+}
