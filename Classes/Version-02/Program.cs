016214bc7ab37a29b867a3e3106a02a21fcb14df
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-25 22:50:24 -0300

    Classes - versão 02

---
+++ Classes/Version-02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.marca = "Ford";
+            carro.modelo = "Ford Fusion";
+            carro.cor = "Preto";
+            carro.ano = 2000;
+            carro.velocidade = 300;
+            Console.WriteLine($"Marca: {carro.marca}\nModelo: {carro.modelo}\nCor: {carro.cor}\nAno: {carro.ano}\nVelocidade: {carro.velocidade}");
+        }
+    }
+
+    class Carro
+    {
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string cor { get; set; }
+        public int ano{ get; set; }
+        public int velocidade { get; set; } 
+    }
+}
