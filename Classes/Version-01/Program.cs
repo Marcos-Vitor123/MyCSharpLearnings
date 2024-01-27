881c5af791a8f0a3eecc82390dd24c27d5cd5f1f
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-26 21:53:58 -0300

    Classes - versão 01 -  Anotações/comentários

--- Classes/Version-01/Program.cs
+++ Classes/Version-01/Program.cs
@@ -16,7 +16,13 @@ namespace Classroom
 
     class Pessoa
     {
-        public string nome { get; set; }
+        public string nome { get; set; } 
         public int idade { get; set; }
     }
 }
+
+// get e set: São usados para acessar e modificar as propriedades de uma classe
+// get: Obtém o valor atual de uma propriedade
+// set: Define um novo valor para essa propriedade
+// Propriedades definidas como públicas: Elas podem ser acessadas e modificadas por qualquer parte do código
+// get e set: São úteis quando precisa controlar o acesso a propriedades
