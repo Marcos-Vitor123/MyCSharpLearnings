7d9104f21d6d13ab3ccfecacdafba48f0e6c830b
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-27 22:49:10 -0300

    Conversão implícita

---
+++ ConversaoImplicita/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConversaoImplicita
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte valorByte = 100;
+            Console.WriteLine(valorByte);
+
+            int valorInteiro = valorByte;
+            Console.WriteLine(valorInteiro);
+
+            char caractere = 'A';
+            Console.WriteLine(caractere);
+
+            int valorInt = caractere;
+            Console.WriteLine(valorInteiro);
+        }
+    }
+}
