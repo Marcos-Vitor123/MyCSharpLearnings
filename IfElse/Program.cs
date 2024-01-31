939eaa07191c9d821be44a6e3c4d31609b39029e
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-30 23:09:06 -0300

    If, Else

---
+++ IfElse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero = 100;
+
+            if (numero > 50)
+            {
+                Console.WriteLine("O número é maior que 50.");
+            }
+            else
+            {
+                Console.WriteLine("O número não é maior que 50.");
+            }
+        }
+    }
+}
