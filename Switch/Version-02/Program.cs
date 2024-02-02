365d5b00128bd3c952cd3e6f32fb49e66e50d3ee
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-01 23:03:53 -0300

    Swicth - Versão 02

---
+++ Switch/Version-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite um número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            switch(numero)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine($"Você digitou: {numero}");
+                    break;
+                default:
+                    Console.WriteLine($"Número inválido!");
+                    break;
+            }
+        }
+    }
+}
