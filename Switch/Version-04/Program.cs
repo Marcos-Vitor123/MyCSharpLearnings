8695845b36976242c537a7a3fecc9622ba5e3d4c
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-01 23:45:41 -0300

    Swicth - Versão 04

---
+++ Switch/Version-04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite um número:");
+
+            string? input = Console.ReadLine();
+
+            if (input != null)
+            {
+                if(int.TryParse(input, out int numero))
+                {
+                   switch(numero)
+                    {
+                        case 1:
+                        case 2:
+                        case 3:
+                            Console.WriteLine($"Você digitou: {numero}");
+                            break;
+                        default:
+                            Console.WriteLine($"Número inválido!");
+                            break;
+                    } 
+                } else
+                {
+                    Console.WriteLine("Entrada inválida!");
+                } 
+            } else
+            {
+                Console.WriteLine("Entrada nula!");
+            } 
+        }
+    }
+}
