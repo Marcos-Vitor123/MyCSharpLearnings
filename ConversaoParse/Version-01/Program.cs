8ea0cbad844ec9477950b0f3c1f6515ae2cc4dc9
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-02 22:23:52 -0300

    Parse - Version-01

---
+++ ConversaoParse/Version-01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string valorString = "123";
+            int valorInteiro = int.Parse(valorString);
+
+            Console.WriteLine(valorInteiro); 
+        }
+    }
+}
