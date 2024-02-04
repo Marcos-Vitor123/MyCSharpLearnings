f099203890f567b99613230c1f43eac97ff1c8bc
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-03 22:59:53 -0300

    For - Version-01

---
+++ For/Version-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"Número: {i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
