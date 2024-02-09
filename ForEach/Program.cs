b1788537632efca1bbcaebe12450e7d8aeb3bfd4
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-08 23:54:49 -0300

    ForEach

---
+++ ForEach/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace forEach
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> frutas = new() {"Maça", "Banana", "Abacaxi", "Pêra"};
+            foreach (string fruta in frutas)
+            {
+                Console.WriteLine(fruta);
+            }
+        }
+    }
+}
