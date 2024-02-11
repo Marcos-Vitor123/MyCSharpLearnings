fd515a015e5adaee27961f55d8795abbd113e01c
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-11 12:52:16 -0300

    Enumeradores - Versão 01

---
+++ Enumeradores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enumeradores
+{
+    internal class Program
+    {
+        enum EDiasDaSemana
+        {
+            Domingo = 1,
+            Segunda = 2,
+            Terça = 3,
+            Quarta = 4,
+            Quinta = 5,
+            Sexta = 6,
+            Sábado = 7
+        }
+        private static void Main(string[] args)
+        {
+            EDiasDaSemana diaDeHoje = EDiasDaSemana.Quinta;
+            Console.WriteLine($"Hoje é: {diaDeHoje}");
+
+            int diaNumero = (int)diaDeHoje;
+            Console.WriteLine($"O Número equivalente a {diaDeHoje} é: {diaNumero}");
+        }
+    }
+}
