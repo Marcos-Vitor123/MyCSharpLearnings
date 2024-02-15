fcff4a3a80ce1ac0565d5c74fcdae7f0074db644
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-15 20:02:32 -0300

    Capturar dados - versão 03

---
+++ CaptureData/Version-03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CaptureData
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+
+            char capturar = Convert.ToChar(Console.ReadLine()); 
+
+            Console.WriteLine($"O número é: {capturar}");
+
+            Console.ReadKey();
+        }
+    }
+}
