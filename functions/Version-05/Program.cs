db660053637f7bcdca301d1dea1667edf89a97cc
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-18 23:46:00 -0300

    Funções - versão 05

---
+++ functions/Version-05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Functions 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string mostrar = NewFunction("Marcos");
+            Console.WriteLine(mostrar);
+        }
+
+        static string NewFunction(string nome)
+        {
+            return nome;
+        }
+    }
+}
