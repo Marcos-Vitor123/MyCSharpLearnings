a6beae93c1de4141ed7a9d2112bb17eaf1c9d34e
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-20 21:44:42 -0300

    Classes - Versão 05

---
+++ Classes/Version-05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Classes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Mostrar();
+        }
+
+        public static string Mostrar()
+        {
+            Empresa minhaEmpresa = new("CEO", "Colaborador");
+
+            Console.WriteLine($"Empresa: {minhaEmpresa.Presidente}\nFuncionário: {minhaEmpresa.Funcionario}");
+
+            return $"Empresa: {minhaEmpresa.Presidente}\nFuncionário: {minhaEmpresa.Funcionario}";
+        }
+    }
+
+    class Empresa
+    {
+        public string Presidente;
+        public string Funcionario;
+
+        public Empresa(string presidente, string funcionario)
+        {
+            Presidente = presidente;
+            Funcionario = funcionario;
+        }
+    }
+}
