3dee1156aa6cea0a8ab7e76a91ed633c1d9a59cf
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-23 23:15:39 -0300

    While - Version-02

---
+++ While/Version-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace While
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int item = 0;
+            int collection = 10;
+
+            while(item < collection)
+            {
+                item++;
+                Console.Clear();
+                Console.WriteLine(item);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Tempo finalizado!");
+        }
+    }
+}
