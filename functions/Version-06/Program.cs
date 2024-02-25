6fe8ab51769158380d78c206a7704aa9022a139b
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-24 22:50:02 -0300

    Functions - Versão 06

---
+++ functions/Version-06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Functions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite qualquer tecla direcional (ou 'S' para sair): ");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                    Console.WriteLine("Para cima");
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                    Console.WriteLine("Para Baixo");
+                else if (keyInfo.Key == ConsoleKey.LeftArrow)
+                    Console.WriteLine("Para esquerda");
+                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                    Console.WriteLine("Para direita");
+                else if (keyInfo.Key == ConsoleKey.S)
+                    break;
+            }
+        }
+    }
+}
