eb50b493b6457baba77c29512f649224e3587da7
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-27 01:44:10 -0300

    Classes - Iniciando - continuação depois.

---
+++ Classes/Version-07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassePrincipal
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite a opção do operador desejado: ");
+            Console.Write("1 - Somar");
+            Console.Write("2 - Subtrair");
+            Console.Write("3 - Multiplicar");
+            Console.Write("4 - Dividir");
+            int operador = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+        }
+    }
+}
