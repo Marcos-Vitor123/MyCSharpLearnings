60d353f60247c1b94736840b5038f76bf0176437
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-27 20:56:46 -0300

    Classes Herança - Classe base

---
+++ ClassesHeranca/Version-02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassesHeranca
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Instância
+            Animal animal = new("Cachorro", 10);
+
+            // Defini a propriedade
+            animal.Nome = "Animal";
+            animal.Idade = 10;
+
+            // Acessa o método
+            animal.EmitirSom();
+
+            Console.WriteLine($"Um animal tem como característica: {animal.Nome}");
+        }
+    }
+
+    // Classe Base
+    public class Animal
+    {
+        // Propriedades
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        // Construtor
+        public Animal (string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        // Métodos
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("Som de um animal qualquer");
+        }
+    }
+}
