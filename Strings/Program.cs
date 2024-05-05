32012c41fbf6289d1bf1d5b4720eda313e5ca023
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-04 23:22:17 -0300

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstFriend = "             Marcos          ";
+            string mySecondFriend = "       Maria                 ";
+
+            myFirstFriend = myFirstFriend.TrimStart();
+            mySecondFriend = mySecondFriend.Trim();
+
+            Console.WriteLine($"{myFirstFriend} and {mySecondFriend}");
+        }
+    }
+}
