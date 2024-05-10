e22f87da0972dd7a19bccd4853b86ac77023fed4
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-09 22:09:08 -0300

    For - Versão 02

---
+++ For/Version-02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace For
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero = 0;
+            Console.Write("Digite o número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < numero + 1; i++)
+            {
+                Console.WriteLine($"Número: {i}");
+            }
+        }
+    }
+}
