62f94d610481327af34140e61e28ad3540cb312c
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-10 19:40:12 -0300

    For - Versão 03

---
+++ For/Version-03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace For
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine($"Número: {i}");
+            }
+        }
+    }
+}
