3c4767c52f247280ba70ecc61e45e9574b691fc3
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-12 22:05:51 -0300

    For - Versão 03.03

--- For/Version-03/Program.cs
+++ For/Version-03/Program.cs
@@ -6,10 +6,12 @@ namespace For
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine($"******************************");
             for (int i = 10; i >= 1; i--)
             {
-                Console.WriteLine($"*************");
+                Console.WriteLine($"*                            *");
             }
+            Console.WriteLine($"******************************");
         }
     }
 }
