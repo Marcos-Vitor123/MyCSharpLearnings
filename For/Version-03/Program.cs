a7f871a9b5dd45903044f1d379bbbd99cc86d35f
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-15 18:24:32 -0300

    For - Versão 03.06 - Imcompleto

--- For/Version-03/Program.cs
+++ For/Version-03/Program.cs
@@ -6,13 +6,33 @@ namespace For
     {
         public static void Main(string[] args)
         {
-            string asteristico = "*                            *";
-            Console.WriteLine($"******************************");
-            for (int i = 10; i >= 1; i--)
+            string asteristico = "*";
+
+            int m = 0;
+            while (m < 31)
             {
                 Console.WriteLine($"{asteristico}");
+                m++;
             }
-            Console.WriteLine($"******************************");
+
+            for (int i = 10; i >= 1; i--)
+            {
+                int n = 0;
+                while (n < 29)
+                {
+                    //Console.WriteLine($"{asteristico}");
+                    Console.Write(" ");
+                    //Console.WriteLine($"{asteristico}");
+                    n++;
+                }
+            }
+
+            int o = 0;
+            while (o < 31)
+            {
+                Console.WriteLine($"{asteristico}");
+                o++;
+            }
         }
     }
 }
