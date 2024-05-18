3944b91e6ffb78852a84e2d95383b9c66bb2f6ef
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-17 21:28:20 -0300

    If Else - version 02

---
+++ IfElse/Version-02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfElse 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Quer saber se o número é positivo ou negativo?\n");
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine("Esse número é positivo.");
+            }
+            else if (numero < 0)
+            {
+                Console.WriteLine("Esse número é negativo.");
+            }
+            else
+            {
+                Console.WriteLine("Esse número é neutro.");
+            }
+        }
+    }
+}
