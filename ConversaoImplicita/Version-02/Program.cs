5a78fd4129d69c100ae16066b8f1d7aacca1c145
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-19 18:25:18 -0300

    Conversão implícita: Version 02

---
+++ ConversaoImplicita/Version-02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConversaoImplicita
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numerosCurto = 123;
+            long numerosCurtoParaLongo = numerosCurto; // Conversão Implícita
+            int numerosLongoParaCurto = (int)numerosCurtoParaLongo; // Conversão Explícita
+        }
+    }
+}
