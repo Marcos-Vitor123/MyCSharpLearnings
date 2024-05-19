b138472c194ac12b0e2153cfec58c1bab9e8fe7b
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-18 23:55:03 -0300

    Enuns: Version 02

---
+++ Enumeradores/Version-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enumeradores
+{
+    enum Season
+    {
+        Spring = 1,
+        Summer = 2,
+        Autumn = 3,
+        Winter = 4
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+           Season currentSeason = Season.Summer;
+           Console.WriteLine($"Estamos na estação: {currentSeason}");
+
+           int seasonIndex = (int)currentSeason;
+           Console.WriteLine($"Índice da estação: {seasonIndex}");
+        }
+    }
+}
