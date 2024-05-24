48bd73998ea6aba8b42966cd4e0844267ffdae65
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-05-23 22:41:54 -0300

    Conversão implícita: Version 02.3

--- ConversaoImplicita/Version-02/Program.cs
+++ ConversaoImplicita/Version-02/Program.cs
@@ -6,15 +6,24 @@ namespace ConversaoImplicita
     {
         private static void Main(string[] args)
         {
-            sbyte numerosDoisDigitos = 12;
-            short numerosQuatroDigitos = 1234;
-            int numerosCurto = 1234567890;
+            sbyte n1 = 123;
+            Console.WriteLine(n1);
+            short n1n2 = n1; // Conversão Implícita
+            Console.WriteLine(n1n2);
+
+            short n2 = 12345;
+            Console.WriteLine(n2);
+            int n2n3 = n2; // Conversão Implícita
+            Console.WriteLine(n2n3);
+
+            int n3 = 1234567890;
+            Console.WriteLine(n3);
+            long n3n4 = n3; // Conversão Implícita
+            Console.WriteLine(n3n4);
+
             //long numerosCurtoParaLongo = numerosCurto; // Conversão Implícita
             //int numerosLongoParaCurto = (int)numerosCurtoParaLongo; // Conversão Explícita
 
-            Console.WriteLine(numerosDoisDigitos);
-            Console.WriteLine(numerosQuatroDigitos);
-            Console.WriteLine(numerosCurto);
             //Console.WriteLine(numerosCurtoParaLongo);
             //Console.WriteLine(numerosLongoParaCurto);
         }
